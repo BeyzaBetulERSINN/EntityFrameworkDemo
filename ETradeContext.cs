6611673434cb6e1ef1120ce6fe12af2af0b63281
Beyza Betül ERSİN <58594224+BeyzaBetulERSINN@users.noreply.github.com>
2021-02-08 15:00:25 +0300

    Initial commit

---
+++ ETradeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class ETradeContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        //Tablolarda producs diye birşey arıyor.
+    }
+}
+//contex birden fazla oluşturulabiliyor ama çoğu projede bir contex ile gidilir
---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class Product // Tablolara karşılık gelen bir nesne
+        //benim bir classım var ve bu class veri tabanında şu class a karşılık geliyor demem lazım.
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int StockAmount { get; set; }
+    }
+}
